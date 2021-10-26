b01524d868bff18438fca01a23cd3e9147c06b4d
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2021-10-25 22:16:29 -0300

    modelo cliente transporte

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -4,5 +4,84 @@ package modelo;
  * @author Gomez Jon Darian, Guardia Lucero Santiago Agustín, Heredia Leandro
  */
 public class Cliente {
+     private int idCliente;
+    private String nombre;
+    private int dni;
+    private String contacto;
+    private int celular;
+     private boolean activo;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliente, String nombre, int dni, String contacto, int celular, boolean activo) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.contacto = contacto;
+        this.celular = celular;
+        this.activo = activo;
+    }
+
+    public Cliente(String nombre, int dni, String contacto, int celular, boolean activo) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.contacto = contacto;
+        this.celular = celular;
+        this.activo = activo;
+    }
+
+     
+     
+     
+     
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
     
 }
--- src/modelo/Transporte.java
+++ src/modelo/Transporte.java
@@ -4,5 +4,82 @@ package modelo;
  * @author Gomez Jon Darian, Guardia Lucero Santiago Agustín, Heredia Leandro
  */
 public class Transporte {
+    private int idTransporte;
+    private String tipodetransporte;
+    private String nombre;
+    private double costo;
+    private Destino destino;
+    private boolean activo;
+
+    public Transporte(int idTransporte, String tipodetransporte, String nombre, double costo, Destino destino, boolean activo) {
+        this.idTransporte = idTransporte;
+        this.tipodetransporte = tipodetransporte;
+        this.nombre = nombre;
+        this.costo = costo;
+        this.destino = destino;
+        this.activo = activo;
+    }
+
+    public Transporte(String tipodetransporte, String nombre, double costo, Destino destino, boolean activo) {
+        this.tipodetransporte = tipodetransporte;
+        this.nombre = nombre;
+        this.costo = costo;
+        this.destino = destino;
+        this.activo = activo;
+    }
+
+    public Transporte() {
+        
+    }
+
+    public int getIdTransporte() {
+        return idTransporte;
+    }
+
+    public void setIdTransporte(int idTransporte) {
+        this.idTransporte = idTransporte;
+    }
+
+    public String getTipodetransporte() {
+        return tipodetransporte;
+    }
+
+    public void setTipodetransporte(String tipodetransporte) {
+        this.tipodetransporte = tipodetransporte;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public Destino getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Destino destino) {
+        this.destino = destino;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
     
 }

237b7fc86844f877fa8397d741d6c69d7be4ec65
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2021-10-26 10:18:19 -0300

    ClienteData y TransporteData

--- src/control/ClienteData.java
+++ src/control/ClienteData.java
@@ -1,8 +1,252 @@
 package control;
 
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.*;
+
 /**
  * @author Gomez Jon Darian, Guardia Lucero Santiago Agustín, Heredia Leandro
  */
 public class ClienteData {
+     private Connection con;
+
+    public ClienteData(Conexion c) { 
+        try {
+            this.con = c.getConexion();
+        } catch (SQLException ex) {
+            System.out.println("Error en la conexion " + ex);
+        }
+
+}
+
+public void AgregarCliente(Cliente c){
+    String sql = "INSERT INTO cliente (nombre,dni,contacto,celular,activo) VALUES (?,?,?,?,?)";
+        try {
+          
+            
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, c.getNombre());
+            ps.setInt(2, c.getDni());
+            ps.setString(3, c.getContacto());
+            ps.setInt(4, c.getCelular());
+            ps.setBoolean(5, c.isActivo());
+            
+            
+ps.executeUpdate();
+ResultSet rs = ps.getGeneratedKeys();
+        } catch (SQLException ex) {
+            System.out.println("Error al agregar un Cliente. " + ex);;
+        }
+}    
+    
+  public void ActualizarCliente(Cliente c){
+        String sql = "UPDATE cliente SET nombre = ?, dni = ?, contacto = ?, celular = ?, activo = ? WHERE id_cliente = ?";
+        
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, c.getNombre());
+            ps.setInt(2, c.getDni());
+            ps.setString(3, c.getContacto());
+            ps.setInt(4, c.getCelular());
+            ps.setBoolean(5, c.isActivo());
+            ps.setInt(6, c.getIdCliente());
+            
+            ps.executeUpdate();
+            ps.close();
+                    
+        }catch(SQLException ex){
+            System.out.println("Error al actualizar el Cliente. " + ex);
+        }
+    }   
+    
+    
+      public Cliente buscarCliente(int idCliente){
+        Cliente c = new Cliente();
+        
+        
+        String sql = "SELECT * FROM cliente WHERE id_cliente = ?";
+        
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idCliente);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                c.setIdCliente(rs.getInt(1));
+                c.setNombre(rs.getString(2));
+                c.setDni(rs.getInt(3));
+                c.setContacto(rs.getString(4));
+                c.setCelular(rs.getInt(5));   
+                c.setActivo(rs.getBoolean(6));
+                
+
+                
+            }
+            
+        }catch(SQLException ex){
+            System.out.println("Error al buscar el Cliente. " + ex);
+        }
+        
+        return c;
+    }
+    
+        public List<Cliente> obtenerClientes(){
+        List<Cliente> clientes = new ArrayList<>();
+        
+        
+        String sql = "SELECT * FROM cliente";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Cliente c = new Cliente();
+                
+                c.setIdCliente(rs.getInt(1));
+                c.setNombre(rs.getString(2));
+                c.setDni(rs.getInt(3));
+                c.setContacto(rs.getString(4));
+                c.setCelular(rs.getInt(5));  
+                
+                c.setActivo(rs.getBoolean(6));
+                
+                
+     clientes.add(c);
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los Clientes. " + ex);
+        }
+        
+        return clientes;
+    }
+    
+    public List<Cliente> obtenerClientesActivos(){
+        List<Cliente> clientes = new ArrayList<>();
+        Cliente c = new Cliente();
+       
+        
+        String sql = "SELECT * FROM cliente WHERE activo = true";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                c.setIdCliente(rs.getInt(1));
+                c.setNombre(rs.getString(2));
+                c.setDni(rs.getInt(3));
+                c.setContacto(rs.getString(4));
+                c.setCelular(rs.getInt(5));  
+                
+                c.setActivo(rs.getBoolean(6));
+                
+                clientes.add(c);
+                
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los Clientes activos. " + ex);
+        }
+        
+        return clientes;
+    }
+        public List<Cliente> obtenerClientesInactivos(){
+        List<Cliente> clientes = new ArrayList<>();
+        Cliente c = new Cliente();
+       
+        
+        String sql = "SELECT * FROM cliente WHERE activo = false";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                c.setIdCliente(rs.getInt(1));
+                c.setNombre(rs.getString(2));
+                c.setDni(rs.getInt(3));
+                c.setContacto(rs.getString(4));
+                c.setCelular(rs.getInt(5));  
+                
+                c.setActivo(rs.getBoolean(6));
+                
+                clientes.add(c);
+                
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los Clientes inactivos. " + ex);
+        }
+        
+        return clientes;
+    }
+        public void desactivarCliente(int idCliente){
+        String sql = "UPDATE cliente SET activo = false WHERE id_cliente = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idCliente);
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al desactivar el cliente. " + ex);
+        }
+    }
+    
+    public void activarCliente(int idCliente){
+        String sql = "UPDATE cliente SET activo = true WHERE id_cliente = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idCliente);
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al activar el cliente. " + ex);
+        }
+    }
+    
+    public void borrarCliente(int idCliente) {
+        String sql = "DELETE FROM cliente WHERE id_cliente = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idCliente);
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al borrar el cliente. " + ex);
+        }
+    }
+    
+    
+    
+    
+    
     
 }
--- src/control/TransporteData.java
+++ src/control/TransporteData.java
@@ -1,8 +1,265 @@
 package control;
 
+import java.sql.*;
+import java.util.*;
+import modelo.*;
+
 /**
  * @author Gomez Jon Darian, Guardia Lucero Santiago Agustín, Heredia Leandro
  */
 public class TransporteData {
+       private Connection con;
+
+    public TransporteData(Conexion t) { 
+        try {
+            this.con = t.getConexion();
+        } catch (SQLException ex) {
+            System.out.println("Error en la conexion " + ex);
+        }
+
+}
+
+public void AgregarTransporte(Transporte t){
+    String sql = "INSERT INTO transporte (tipo_transporte,nombre,costo,id_destino,activo) VALUES (?,?,?,?,?)";
+        try {
+          
+            
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, t.getTipodetransporte());
+            ps.setString(2, t.getNombre());
+            ps.setDouble(3, t.getCosto());
+            ps.setInt(4, t.getDestino().getIdDestino());
+            ps.setBoolean(5, t.isActivo());
+            
+            
+            
+ps.executeUpdate();
+ResultSet rs = ps.getGeneratedKeys();
+        } catch (SQLException ex) {
+            System.out.println("Error al agregar un transporte. " + ex);;
+        }
+}    
     
+  public void ActualizarTransporte(Transporte t){
+        String sql = "UPDATE transporte SET tipo_transporte = ?, nombre = ?, costo = ?, id_destino = ?, activo = ? WHERE id_transporte = ?";
+        
+        try{
+            
+            
+           PreparedStatement ps = con.prepareStatement(sql);
+           
+            ps.setString(1, t.getTipodetransporte());
+            ps.setString(2, t.getNombre());
+            ps.setDouble(3, t.getCosto());
+            ps.setInt(4, t.getDestino().getIdDestino());
+            ps.setBoolean(5, t.isActivo());
+
+            
+            ps.executeUpdate();
+            ps.close();
+                    
+        }catch(SQLException ex){
+            System.out.println("Error al actualizar el Transporte. " + ex);
+        }
+    }   
+    
+    
+      public Transporte buscarTransporte(int idTransporte){
+        Transporte t = new Transporte();
+        Destino destino;
+        
+        String sql = "SELECT * FROM transporte WHERE id_transporte = ?";
+        
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idTransporte);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                
+                t.setIdTransporte(rs.getInt(1));
+                t.setTipodetransporte(rs.getString(2));
+                t.setNombre(rs.getString(3));
+                t.setCosto(rs.getDouble(4));
+                destino = buscarDestino(rs.getInt(5));
+                t.setActivo(rs.getBoolean(6));
+                
+                
+                t.setDestino(destino);
+           
+                
+            }
+            
+        }catch(SQLException ex){
+            System.out.println("Error al buscar el Transporte. " + ex);
+        }
+        
+        return t;
+    }
+    
+        public List<Transporte> obtenerTransporte(){
+        List<Transporte> transportes = new ArrayList<>();
+        Destino destino;
+        
+        String sql = "SELECT * FROM transporte";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Transporte t = new Transporte();
+                
+               t.setIdTransporte(rs.getInt(1));
+                t.setTipodetransporte(rs.getString(2));
+                t.setNombre(rs.getString(3));
+                t.setCosto(rs.getDouble(4));
+                destino = buscarDestino(rs.getInt(5));
+                t.setActivo(rs.getBoolean(6));
+                
+                
+                t.setDestino(destino);
+                
+                
+     transportes.add(t);
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los transportes. " + ex);
+        }
+        
+        return transportes;
+    }
+    
+    public List<Transporte> obtenerTransportesActivos(){
+        List<Transporte> transportes = new ArrayList<>();
+        Transporte t = new Transporte();
+       Destino destino;
+        
+        String sql = "SELECT * FROM transporte WHERE activo = true";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                 t.setIdTransporte(rs.getInt(1));
+                t.setTipodetransporte(rs.getString(2));
+                t.setNombre(rs.getString(3));
+                t.setCosto(rs.getDouble(4));
+                destino = buscarDestino(rs.getInt(5));
+                t.setActivo(rs.getBoolean(6));
+                
+                
+                t.setDestino(destino);
+                
+                transportes.add(t);
+                
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los Transportes activos. " + ex);
+        }
+        
+        return transportes;
+    }
+        public List<Transporte> obtenerTrasportesInactivos(){
+        List<Transporte> transportes = new ArrayList<>();
+        Transporte t = new Transporte();
+        Destino destino;
+        
+        String sql = "SELECT * FROM transporte WHERE activo = false";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                t.setIdTransporte(rs.getInt(1));
+                t.setTipodetransporte(rs.getString(2));
+                t.setNombre(rs.getString(3));
+                t.setCosto(rs.getDouble(4));
+                destino = buscarDestino(rs.getInt(5));
+                t.setActivo(rs.getBoolean(6));
+                
+                
+                t.setDestino(destino);
+                
+                transportes.add(t);
+                
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los Trasportes inactivos. " + ex);
+        }
+        
+        return transportes;
+    }
+        public void desactivarTransportes(int idTransporte){
+        String sql = "UPDATE transporte SET activo = false WHERE id_transporte = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idTransporte);
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al desactivar el Transporte. " + ex);
+        }
+    }
+    
+    public void activarTransportes(int idTransporte){
+        String sql = "UPDATE transporte SET activo = true WHERE id_transporte = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idTransporte);
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al activar el Transporte. " + ex);
+        }
+    }
+    
+    public void borrarTransporte(int idTransporte) {
+        String sql = "DELETE FROM transporte WHERE id_transporte = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, idTransporte);
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al borrar el transporte. " + ex);
+        }
+    }
+    
+    // Buscador
+    public Destino buscarDestino(int  idDestino) {
+    
+        DestinoData des = new DestinoData((Conexion) con);
+   
+    
+    return des.buscarDestino(idDestino);
+    }
 }
